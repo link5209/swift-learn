b7ec4d01e87051fa149c63b8d4bf9705b6df45d0
link5209 <195140532@qq.com>
2018-11-14 11:14:01 +0800

    inherit ChannelInboundHandler for use self

--- Sources/NonBlockingFileIO-learn/main.swift
+++ Sources/NonBlockingFileIO-learn/main.swift
@@ -8,34 +8,42 @@ threadPool.start()
 let fileIO = NonBlockingFileIO(threadPool: threadPool)
 let filePath = "/Users/jack/swift-learn/Tests/LinuxMain.swift"
 
-func sendfile(_ ctx: ChannelHandlerContext, _ reqPart: HTTPServerRequestPart) {
-    switch reqPart {
-    case .head(let request):
-        let fileHandleAndRegion = fileIO.openFile(path: filePath, eventLoop: ctx.eventLoop)
-        fileHandleAndRegion.whenFailure {err in
+class FileNIO: ChannelInboundHandler {
+    typealias InboundIn = HTTPServerRequestPart
+    public typealias OutboundOut = HTTPServerResponsePart
 
+    func sendfile(_ ctx: ChannelHandlerContext, _ reqPart: HTTPServerRequestPart) {
+        switch reqPart {
+        case .head(let request):
+            let fileHandleAndRegion = fileIO.openFile(path: filePath, eventLoop: ctx.eventLoop)
+            fileHandleAndRegion.whenFailure {err in
+
+            }
+            fileHandleAndRegion.whenSuccess {file, region in
+                var head = HTTPResponseHead(version: request.version, status: .ok)
+                head.headers.add(name: "Content-Length", value: "\(region.endIndex)")
+                head.headers.add(name: "Content-Type", value: "text/plain; charset=utf-8")
+                ctx.write(self.wrapOutboundOut(.head(head)), promise: nil)
+
+                // 使用promise异步，避免阻塞eventLoop
+                ctx.write(self.wrapOutboundOut(.body(.fileRegion(region)))).then {
+                    ctx.writeAndFlush(self.wrapOutboundOut(.end(nil)), promise: nil)
+
+                    let p: EventLoopPromise<Void> = ctx.eventLoop.newPromise()
+                    p.futureResult.whenComplete {ctx.close(promise: nil)}
+                    return p.futureResult
+                }
+                .thenIfError {err in
+                    ctx.close()
+                }
+                .whenComplete {
+                    
+                }
+
+            }
+        case .body, .end:
+            break
         }
-        fileHandleAndRegion.whenSuccess {file, region in
-            var head = HTTPResponseHead(version: request.version, status: .ok)
-            head.headers.add(name: "Content-Length", value: "\(region.endIndex)")
-            head.headers.add(name: "Content-Type", value: "text/plain; charset=utf-8")
-            ctx.write(wrapOutboundOut(.head(head)), promise: nil)
-
-            // 使用promise异步，避免阻塞eventLoop
-            ctx.write(wrapOutboundOut(.body(.fileRegion(region)))).then {
-
-            }
-            .whenComplete {
-                ctx.writeAndFlush(wrapOutboundOut(.end(nil)), promise: nil)
-                
-            }
-            .thenIfError {
-                ctx.close()
-            }
-
-        }
-    case .body, .end:
-        break
     }
-
 }
+
